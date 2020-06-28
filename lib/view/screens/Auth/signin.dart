7629fb61f434e1151baabe2b132061a2a1c297eb
Your Name <you@example.com>
2020-06-28 20:43:43 +0200

    alertdialog widget

--- lib/view/screens/Auth/signin.dart
+++ lib/view/screens/Auth/signin.dart
@@ -3,6 +3,7 @@ import 'package:Oglasnik/view/screens/AnonymousHome/anonymousHome.dart';
 import 'package:Oglasnik/view/screens/Auth/register.dart';
 import 'package:Oglasnik/view/screens/RegisterHome/registeredHome.dart';
 import 'package:Oglasnik/view/screens/PasswordChange/passwordChange.dart';
+import 'package:Oglasnik/view/widgets/alertdialog.dart';
 import 'package:Oglasnik/view/widgets/logoContainer.dart';
 import 'package:Oglasnik/view/widgets/specialElements.dart';
 import 'package:Oglasnik/viewModel/authViewModel.dart';
@@ -178,7 +179,7 @@ class _SigninPageState extends State<SigninPage> {
         Container(
           margin: EdgeInsets.only(top: 15.0),
           child: new GestureDetector(
-            onTap: () => _displayDialog(context),
+            onTap: () => displayDialog(context),
             child: new Text(
               "Zaboravili ste lozinku?",
               style: TextStyle(
@@ -228,52 +229,4 @@ class _SigninPageState extends State<SigninPage> {
       ),
     );
   }
-}
-
-_displayDialog(BuildContext context) async {
-  return showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          backgroundColor: Color.fromARGB(255, 239, 232, 232),
-          shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10.0),
-              side: BorderSide(color: Color.fromRGBO(112, 112, 112, 100))),
-          content: TextFormField(
-            decoration: InputDecoration(
-              hintText: 'Email',
-            ),
-            keyboardType: TextInputType.emailAddress,
-          ),
-          actions: <Widget>[
-            new FlatButton(
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(2.0)),
-              color: Color.fromARGB(255, 226, 11, 48),
-              child: new Text(
-                'ODUSTANI',
-                style: TextStyle(
-                    color: Colors.white, fontFamily: 'Roboto', fontSize: 14),
-              ),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-            new FlatButton(
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(2.0)),
-              color: Color.fromARGB(255, 226, 11, 48),
-              child: new Text(
-                'POŠALJI KOD',
-                style: TextStyle(
-                    color: Colors.white, fontFamily: 'Roboto', fontSize: 14),
-              ),
-              onPressed: () => Navigator.of(context)
-                  .pushReplacement(MaterialPageRoute(builder: (_) {
-                return PasswordChange();
-              })),
-            ),
-          ],
-        );
-      });
 }
