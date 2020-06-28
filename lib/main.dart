1614f9e663fae88a0bf05871fb46e55b165c56bd
Your Name <you@example.com>
2020-06-28 16:25:04 +0200

    added alert dialog screen(design not yet fully finshed),and added change password screen(minor design changes needed and validation for emn

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:Oglasnik/view/screens/AnonymousHome/anonymousHome.dart';
 import 'package:Oglasnik/view/screens/Auth/register.dart';
 import 'package:Oglasnik/view/screens/Auth/alertdialog.dart';
 import 'package:Oglasnik/view/screens/Auth/signin.dart';
+import 'package:Oglasnik/view/screens/PasswordChange/passwordChange.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -51,7 +52,7 @@ class MyApp extends StatelessWidget {
           textTheme: ThemeData.light()
               .textTheme
               .copyWith(headline6: TextStyle(fontFamily: 'Roboto'))),
-      home: RegisterPage(),
+      home: SigninPage(),
     );
   }
 }
