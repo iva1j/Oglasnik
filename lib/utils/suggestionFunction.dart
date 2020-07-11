05e36bf0bcf79743b1f32cb04901ecb8cc0f4b4f
Chiida <faruk.cidic@gmail.com>
2020-07-11 23:22:43 +0200

    Suggestion Box finished

---
+++ lib/utils/suggestionFunction.dart
@@ -0,0 +1,28 @@
+class CitiesService {
+  static final List<String> cities = [
+    'Beirut',
+    'Damascus',
+    'San Fransisco',
+    'Rome',
+    'Los Angeles',
+    'Madrid',
+    'Bali',
+    'Barcelona',
+    'Paris',
+    'Bucharest',
+    'New York City',
+    'Philadelphia',
+    'Sydney',
+  ];
+
+  static List<String> getSuggestions(String query) {
+    List<String> matches = List();
+    matches.addAll(cities);
+
+    matches.retainWhere((s) => s.toLowerCase().contains(query.toLowerCase()));
+    if (query.length >= 3)
+      return matches;
+    else
+      return null;
+  }
+}
