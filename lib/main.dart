23a655d2eb4235659ed7201423d297970e324f2e
Amer Hero <amherhero@gmail.com>
2020-07-13 20:17:33 +0200

    nestonesto

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,7 @@ class MyApp extends StatelessWidget {
           title: 'Flutter Demo',
           theme: buildThemeData(),
           // ignore: missing_required_param
-          home: Splash(),
+          home: AnonymousHome(),
           routes: {
             "/back": (_) => AnonymousHome(),
             "/homeregister": (_) => RegisteredHome(),
