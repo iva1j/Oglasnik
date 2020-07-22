48b04502fc05fbda17e733b411869afd56b9d2e8
Chiida <faruk.cidic@gmail.com>
2020-07-22 06:23:38 +0200

    Updating containers position and margins on ItemCard

--- lib/view/RegisterHome/widgets/ProductsCards/itemCard.dart
+++ lib/view/RegisterHome/widgets/ProductsCards/itemCard.dart
@@ -1,6 +1,7 @@
 import 'package:Oglasnik/utils/shared/globalVariables.dart';
 import 'package:Oglasnik/utils/strings.dart';
 import 'package:Oglasnik/view/RegisterHome/widgets/ProductsCards/productDetails.dart';
+import 'package:Oglasnik/view/RegisterHome/widgets/mainFloatingButton.dart';
 import 'package:flutter/material.dart';
 import 'package:Oglasnik/utils/sizeconfig.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -19,140 +20,161 @@ class ItemCard extends StatelessWidget {
           centerTitle: true,
           title: Text(brandNameScreen),
         ),
-        body: StreamBuilder(
-            stream: Firestore.instance
-                .collection('products')
-                .where('productBrand', isEqualTo: brandNameScreen)
-                .snapshots(),
-            builder: (context, snapshot) {
-              if (!snapshot.hasData) {
-                return Container(
-                  child: Card(
-                    elevation: 8,
-                    color: Colors.white,
-                    child: Text('No Items'),
-                  ),
-                );
-              }
-              return ListView.builder(
-                itemCount: snapshot.data.documents.length,
-                itemBuilder: (_, int index) {
-                  return Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.2),
-                          spreadRadius: 1,
-                          blurRadius: 7,
-
-                          offset: Offset(0, 3), // changes position of shadow
+        floatingActionButton: mainFloatingButton(email),
+        bottomSheet: Container(
+          height: 55,
+          width: double.infinity,
+          color: Color.fromARGB(255, 226, 11, 48),
+        ),
+        body: Column(
+          children: <Widget>[
+            SizedBox(
+              height: SizeConfig.blockSizeVertical * 10,
+            ),
+            Expanded(
+              child: StreamBuilder(
+                  stream: Firestore.instance
+                      .collection('products')
+                      .where('productBrand', isEqualTo: brandNameScreen)
+                      .snapshots(),
+                  builder: (context, snapshot) {
+                    if (!snapshot.hasData) {
+                      return Container(
+                        child: Card(
+                          elevation: 8,
+                          color: Colors.white,
+                          child: Text('No Items'),
                         ),
-                      ],
-                      border: Border.all(
-                        color: Colors.black26,
-                      ),
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(4),
-                      ),
-                    ),
-                    margin: EdgeInsets.symmetric(
-                      horizontal: SizeConfig.blockSizeHorizontal * 5,
-                      vertical: SizeConfig.blockSizeVertical * 10,
-                    ),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: <Widget>[
-                            Expanded(
-                              child: Column(
+                      );
+                    }
+                    return ListView.builder(
+                      itemCount: snapshot.data.documents.length,
+                      itemBuilder: (_, int index) {
+                        return Container(
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.2),
+                                spreadRadius: 1,
+                                blurRadius: 7,
+
+                                offset:
+                                    Offset(0, 3), // changes position of shadow
+                              ),
+                            ],
+                            border: Border.all(
+                              color: Colors.black26,
+                            ),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(4),
+                            ),
+                          ),
+                          margin: EdgeInsets.only(
+                            left: SizeConfig.blockSizeHorizontal * 5,
+                            right: SizeConfig.blockSizeHorizontal * 5,
+                            //top: SizeConfig.blockSizeVertical * 5,
+                            bottom: SizeConfig.blockSizeVertical * 5,
+                          ),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.start,
                                 mainAxisAlignment: MainAxisAlignment.start,
                                 children: <Widget>[
-                                  InkWell(
-                                    onTap: () {
-                                      Navigator.of(context)
-                                          .push(MaterialPageRoute(
-                                              builder: (_) => ProductDetails(
-                                                    productNameScreen: snapshot
-                                                            .data
-                                                            .documents[index]
-                                                        ['productName'],
-                                                  )));
-                                    },
-                                    child: ItemName(
-                                      name: snapshot.data.documents[index]
-                                          ['productName'],
+                                  Expanded(
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      children: <Widget>[
+                                        InkWell(
+                                          onTap: () {
+                                            Navigator.of(context).push(
+                                                MaterialPageRoute(
+                                                    builder: (_) =>
+                                                        ProductDetails(
+                                                          productNameScreen: snapshot
+                                                                      .data
+                                                                      .documents[
+                                                                  index]
+                                                              ['productName'],
+                                                        )));
+                                          },
+                                          child: ItemName(
+                                            name: snapshot.data.documents[index]
+                                                ['productName'],
+                                          ),
+                                        ),
+                                        ItemDescription(
+                                          description: snapshot.data
+                                              .documents[index]['productDesc'],
+                                        ),
+                                      ],
                                     ),
                                   ),
-                                  ItemDescription(
-                                    description: snapshot.data.documents[index]
-                                        ['productDesc'],
+                                  Expanded(
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.stretch,
+                                      children: <Widget>[
+                                        ItemImage(
+                                            /*
+                                            img: snapshot.data.documents[index]
+                                                        ['productImg1'] !=
+                                                    null
+                                                ? snapshot.data.documents[index]
+                                                    ['productImg1']
+                                                : ""*/
+                                            img: (snapshot.data.documents[index]
+                                                        ['productImg1'] !=
+                                                    null)
+                                                ? snapshot.data.documents[index]
+                                                    ['productImg1']
+                                                : (snapshot.data.documents[index]
+                                                            ['productImg2'] !=
+                                                        null)
+                                                    ? snapshot.data.documents[index]
+                                                        ['productImg2']
+                                                    : (snapshot.data.documents[index][
+                                                                'productImg3'] !=
+                                                            null)
+                                                        ? snapshot.data.documents[index]
+                                                            ['productImg3']
+                                                        : ""),
+                                        ItemPrice(
+                                            price: snapshot.data
+                                                .documents[index]['cijena']),
+                                      ],
+                                    ),
                                   ),
                                 ],
                               ),
-                            ),
-                            Expanded(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.end,
-                                children: <Widget>[
-                                  ItemImage(
-                                      /*
-                                      img: snapshot.data.documents[index]
-                                                  ['productImg1'] !=
-                                              null
-                                          ? snapshot.data.documents[index]
-                                              ['productImg1']
-                                          : ""*/
-                                      img: (snapshot.data.documents[index]
-                                                  ['productImg1'] !=
-                                              null)
-                                          ? snapshot.data.documents[index]
-                                              ['productImg1']
-                                          : (snapshot.data.documents[index]
-                                                      ['productImg2'] !=
-                                                  null)
-                                              ? snapshot.data.documents[index]
-                                                  ['productImg2']
-                                              : (snapshot.data.documents[index]
-                                                          ['productImg3'] !=
-                                                      null)
-                                                  ? snapshot
-                                                          .data.documents[index]
-                                                      ['productImg3']
-                                                  : ""),
-                                  ItemPrice(
-                                      price: snapshot.data.documents[index]
-                                          ['cijena']),
-                                ],
+                              Divider(
+                                thickness: SizeConfig.blockSizeVertical * 0.2,
                               ),
-                            ),
-                          ],
-                        ),
-                        Divider(
-                          thickness: SizeConfig.blockSizeVertical * 0.2,
-                        ),
-                        SingleChildScrollView(
-                          scrollDirection: Axis.horizontal,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: snapshot
-                                .data.documents[index]['productTag']
-                                .split(',')
-                                .map<Widget>(
-                                    (element) => new OglasTag(naziv: element))
-                                .toList(),
+                              SingleChildScrollView(
+                                scrollDirection: Axis.horizontal,
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: snapshot
+                                      .data.documents[index]['productTag']
+                                      .split(',')
+                                      .map<Widget>((element) =>
+                                          new OglasTag(naziv: element))
+                                      .toList(),
+                                ),
+                              ),
+                            ],
                           ),
-                        ),
-                      ],
-                    ),
-                  );
-                },
-              );
-            }));
+                        );
+                      },
+                    );
+                  }),
+            ),
+          ],
+        ));
   }
 }
 
@@ -198,7 +220,7 @@ class ItemDescription extends StatelessWidget {
       margin: EdgeInsets.only(
         top: SizeConfig.blockSizeVertical * 2,
         bottom: SizeConfig.blockSizeVertical,
-        // left: SizeConfig.blockSizeHorizontal * 3,
+        left: SizeConfig.blockSizeHorizontal * 3,
       ),
       child: Text(
         description,
@@ -246,8 +268,9 @@ class ItemPrice extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.only(
-          top: SizeConfig.blockSizeVertical,
-          right: SizeConfig.blockSizeHorizontal * 3),
+        top: SizeConfig.blockSizeVertical,
+        // right: SizeConfig.blockSizeHorizontal * 3
+      ),
       child: Text(
         NumberFormat.currency(locale: 'eu', symbol: 'KM')
             .format((double.parse(price))),
@@ -255,6 +278,7 @@ class ItemPrice extends StatelessWidget {
           fontSize: SizeConfig.safeBlockHorizontal * 5,
           fontWeight: FontWeight.w700,
         ),
+        textAlign: TextAlign.center,
       ),
     );
   }
