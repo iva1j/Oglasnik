bd95a77e84a7cde39e9a829f48f3e2fe68689eeb
Your Name <you@example.com>
2020-07-28 23:03:23 +0200

    models refaktorisan

---
+++ lib/view/AnonymousHome/pages/mainbody.dart
@@ -0,0 +1,13 @@
+import 'package:Oglasnik/view/RegisterHome/widgets/ProductsCards/categoryCard.dart';
+import 'package:flutter/material.dart';
+
+class MainBody extends StatelessWidget {
+  const MainBody({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CategoryCard();
+  }
+}
