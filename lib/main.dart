c914783f6e84d01be387371a4ccc19ff04984691
Amer Hero <amherhero@gmail.com>
2020-08-03 09:30:11 +0200

    Razdvojen spinner na kategorijama i kreiranju novog artikla

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:Oglasnik/utils/lifecycle_manager.dart';
 import 'package:Oglasnik/utils/shared/checkingInternetConnection/internetAlertDialog.dart';
 import 'package:Oglasnik/view/AnonymousHome/pages/anonymousHome.dart';
 import 'package:Oglasnik/view/RegisterHome/pages/registeredHome.dart';
+import 'package:Oglasnik/view/RegisterHome/widgets/ProductsCards/categoryLoading.dart';
 import 'package:Oglasnik/view/RegistrationPageAuth/pages/register.dart';
 import 'package:Oglasnik/view/SignInPage/pages/signin.dart';
 import 'package:Oglasnik/view/SplashScreen/pages/splash.dart';
@@ -40,7 +41,7 @@ class MyApp extends StatelessWidget {
           debugShowCheckedModeBanner: false,
           title: 'Flutter Demo',
           theme: buildThemeData(),
-          home: Splash(),
+          home: CategoryLoading(),
           routes: {
             "/back": (_) => AnonymousHome(),
             "/homeregister": (_) => RegisteredHome(),
