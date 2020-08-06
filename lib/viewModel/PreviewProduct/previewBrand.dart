e686fd2b0345ef698c7033f2b259095fa4d7e135
Elvir9 <ehalilovicc@gmail.com>
2020-08-06 11:19:14 +0200

    adding comments on this files

--- lib/viewModel/PreviewProduct/previewBrand.dart
+++ lib/viewModel/PreviewProduct/previewBrand.dart
@@ -4,6 +4,12 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class BrandViewModel implements ReadBrandInterface {
   @override
+
+  /// U sljedecim linijama code-a vidimo logiku koja se zasniva na sljedecem
+  /// Future metoda getBrandsByCategories prima kao parametar categoryName, a koja
+  /// je ujedno async metoda, vrsi upit nad bazom i dohvaca neke podatke.
+  /// Zamisao je da dohvati kolekciju pod imenom products gdje je dokument productCategory
+  /// jednak polju categoryName i to se smjesta u listu ispod
   Future getBrandsByCategories(String categoryName) async {
     var firestore = Firestore.instance;
     QuerySnapshot qn = await firestore
@@ -13,6 +19,9 @@ class BrandViewModel implements ReadBrandInterface {
 
     List<DocumentSnapshot> l = qn.documents;
 
+    /// Sljedece linije code-a objasnjavaju poredjenje sortirane vrijednosti iz liste
+    /// Logika se zasniva na brojacu koji ima uslov da ide od pocetka do kraja liste te
+    /// koji se povecava ili brise u zavisnosti da li imamo poredjenje vrijednosti ili nemamo
     l.sort((a, b) => a['productBrand'].compareTo(b['productBrand']));
     int i = 0;
     while (i < l.length - 1) {
