b3010513b9aa426acd3aa26d2eecc34bac668918
Fahrudin <therealfahrudinsljivo@gmail.com>
2020-08-09 00:00:34 +0200

    image compression works

--- lib/viewModel/ImageUpload/upload.dart
+++ lib/viewModel/ImageUpload/upload.dart
@@ -1,12 +1,15 @@
 import 'dart:io';
 
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter_native_image/flutter_native_image.dart';
 
 Future<String> upload(fileName, filePath, id) async {
   StorageReference storageRef =
       FirebaseStorage.instance.ref().child('images').child(fileName);
+  File compressedFile = await FlutterNativeImage.compressImage(filePath,
+      quality: 50, percentage: 50);
   final StorageUploadTask task = storageRef.putFile(
-    File(filePath),
+    compressedFile,
   );
   return await (await task.onComplete).ref.getDownloadURL();
 }
