0e27c2e996fbf8ebfe679c19e503ee976fe96fc2
Your Name <you@example.com>
2020-08-18 16:19:37 +0200

    weirdly upload working

--- lib/viewModel/ImageUpload/upload.dart
+++ lib/viewModel/ImageUpload/upload.dart
@@ -2,20 +2,14 @@ import 'dart:io';
 
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter_native_image/flutter_native_image.dart';
-import 'package:flutter_image_compress/flutter_image_compress.dart';
 
 Future<String> upload(fileName, filePath, id) async {
   StorageReference storageRef =
       FirebaseStorage.instance.ref().child('images').child(fileName);
-  final lastIndex = filePath.lastIndexOf(new RegExp(r'.jp'));
-  final splitted = filePath.substring(0, (lastIndex));
-  final outPath = "${splitted}_out${filePath.substring(lastIndex)}";
-
-  final compressedImage = await FlutterImageCompress.compressAndGetFile(
-      filePath, outPath,
-      minWidth: 1000, minHeight: 1000, quality: 70);
+  File compressedFile = await FlutterNativeImage.compressImage(filePath,
+      quality: 50, percentage: 50);
   final StorageUploadTask task = storageRef.putFile(
-    compressedImage,
+    compressedFile,
   );
   return await (await task.onComplete).ref.getDownloadURL();
 }
